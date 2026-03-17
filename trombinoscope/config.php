0215b394b034dff5dd957d2e80d26e9361da701c
Basti1-boop <bastienberceron@gmail.com>
2026-03-17 14:05:14 +0100

    Update config.php

--- trombinoscope/config.php
+++ trombinoscope/config.php
@@ -1,8 +1,5 @@
 <?php
-/*Avant toute chose, créez un zchier config.php qui établit la connexion PDO à votre base de données. Ce
-zchier sera inclus dans toutes les pages qui en ont besoin avec require_once .
-Utilisez PDO avec le mode d'erreur PDO::ERRMODE_EXCEPTION . Cherchez dans la documentation PHP
-comment instancier une connexion PDO et déznir son mode d'erreur. */
+
 $host = 'localhost'; 
 $db = 'trombinoscope'; 
 $user = 'root'; 
