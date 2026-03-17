1ad71061e016c28d73ebffc5acb30447c157b446
Basti1-boop <bastienberceron@gmail.com>
2026-03-17 13:21:53 +0100

    first commit

---
+++ trombinoscope/config.php
@@ -0,0 +1,6 @@
+<?php
+/*Avant toute chose, créez un zchier config.php qui établit la connexion PDO à votre base de données. Ce
+zchier sera inclus dans toutes les pages qui en ont besoin avec require_once .
+Utilisez PDO avec le mode d'erreur PDO::ERRMODE_EXCEPTION . Cherchez dans la documentation PHP
+comment instancier une connexion PDO et déznir son mode d'erreur. */
+?>
